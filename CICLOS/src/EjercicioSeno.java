a7f5e69374b42f58cdc8175cb1e5507cd41c2ffe
Teleinformatica III <Teleinformatica III@FDBDFPCISXF610.FormacionSena.RED>
2019-12-11 09:12:55 -0500

    ok

---
+++ CICLOS/src/EjercicioSeno.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class EjercicioSeno {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int termino=0;
+        float angulo=0;
+        float serie=0;
+        System.out.println("Digite el valor del angulo en radianes");
+        angulo = leer.nextFloat();
+        System.out.println("Digite el valor del termino de la serie");
+        termino=leer.nextInt();
+        boolean par=true;
+        int i=0;
+        while(termino>0) {
+            if (par==true) { //if ((i%2)==0)
+                serie=serie+((float) Math.pow(angulo, 2*i+1)/factorial(2*i+1));
+                par=false;
+            } else {
+                serie=serie-((float) Math.pow(angulo, 2*i+1)/factorial(2*i+1));
+                par=true;
+            }
+            i++;
+            termino--;
+        }
+    }
+
+    private static long factorial(int n) {
+        long fact=1;
+        for (int i=1; i<=n; i++) {
+            fact=fact*i;
+        }
+        return fact;
+    }
+    
+}
--- CICLOS/src/Ramdimicos.java
+++ CICLOS/src/Ramdimicos.java
@@ -1,10 +1,11 @@
 public class Ramdimicos {
     public static void main(String[] args) {
-        for (int i=0; i<100; i++) {
+        /*for (int i=0; i<100; i++) {
             int edad = ((int)(Math.random()*16))+10;
             System.out.print(edad);
             int sexo = ((int)(Math.random()*2))+1;
             System.out.println(" "+((sexo==1)?'M':'F'));
-        }
+        }*/
+        System.out.println(3%2);
     }
 }
